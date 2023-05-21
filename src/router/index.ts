ff971b2f023ba1a4613f3d4beaf13c0d1a25a296
Michael Rockingham <mrockingham2@gmail.com.com>
2023-05-21 15:10:30 -0400

    update cors

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import user from './userRoutes/user'
 import editStyle from './editStylesRoutes/editStyles'
 import images from './imagesRoutes/images'
+import cors from 'cors';
 
 
 const app = express();
-
+app.use(cors());
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
     res.status(200).json({ api: 'App blog is UP' });
