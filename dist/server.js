dc49bdd7f3f892fcaa5db349f22ec9f578f70570
Michael Rockingham <mrockingham2@gmail.com.com>
2023-12-18 15:28:50 -0500

    connection test

--- dist/server.js
+++ dist/server.js
@@ -15,7 +15,7 @@ const server = (0, express_1.default)();
 server.use((0, cors_1.default)());
 const port = process.env.PORT;
 server.options('*', (0, cors_1.default)());
-(0, config_1.default)();
+// (0, config_1.default)();
 dotenv_1.default.config();
 server.use(express_1.default.json());
 server.use(express_1.default.urlencoded({ extended: true }));
@@ -24,6 +24,7 @@ server.use((0, helmet_1.default)());
 server.use('/app', index_1.default);
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'blog is UP' });
+
   server.listen(port, () =>
     console.log(`\n** Server is listening on the port ${port} **\n`)
   );
